8e5fc819d6e27287bd6bb4e28dbc82538a13c0f1
Sebastian Muñoz Riquelme <manredfan10@gmail.com>
2023-05-25 22:50:52 -0400

    Background

--- Proyecto UCTTOOLS/src/pages/Horario.js
+++ Proyecto UCTTOOLS/src/pages/Horario.js
@@ -4,27 +4,30 @@ import './horario.css';
 const Horario = () =>{
 
     return (
-      <div class="content">
-        <h1 class="title">
-          Creador de Horario
-        </h1>
+    
+      <body>
+        <div class="content">
+          <h1 class="title">
+            Creador de Horario
+          </h1>
 
-        <div>
-          Nombre del Ramo:<input class="input" type="text" placeholder=""></input>
+          <div>
+            Nombre del Ramo:<input class="input" type="text" placeholder=""></input>
+          </div>
+
+          <div>
+            Hora de Inicio y Final:<input class="input" type="text" placeholder=""></input>
+          </div>
+
+          <div>
+            Docente Encargado:<input class="input" type="text" placeholder=""></input>
+          </div>
+
+          <button class="btn-agregar">Agregar Clase</button>
+          <button class="btn-borrar">Borrar Clase</button>
+
         </div>
-
-        <div>
-          Hora de Inicio y Final:<input class="input" type="text" placeholder=""></input>
-        </div>
-
-        <div>
-          Docente Encargado:<input class="input" type="text" placeholder=""></input>
-        </div>
-
-        <button class="btn-agregar">Agregar Clase</button>
-        <button class="btn-borrar">Borrar Clase</button>
-
-      </div>
+      </body>
     );
 }
 
--- Proyecto UCTTOOLS/src/pages/Salas.js
+++ Proyecto UCTTOOLS/src/pages/Salas.js
@@ -4,14 +4,17 @@ import './salas.css';
 const Salas = () =>{
 
     return (
-      <div class="content">
-        <h1 class="title">
-          Buscador de Salas
-        </h1>
-        
-        <input class="input" type="text" placeholder="Ingrese la sala que desea buscar"></input>
-        <button id = "btn" class="fa fa-magnifying-glass"></button>
-      </div>
+
+      <body>
+        <div class="content">
+          <h1 class="title">
+            Buscador de Salas
+          </h1>
+          
+          <input class="input" type="text" placeholder="Ingrese la sala que desea buscar"></input>
+          <button id = "btn" class="fa fa-magnifying-glass"></button>
+        </div>
+      </body>
     );
 }
 
